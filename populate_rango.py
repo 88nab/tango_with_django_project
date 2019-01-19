7aa6dba91b100df0bc4e93b68c238979a6d10f8c
Nicki Buckley <0602452b@student.gla.ac.uk>
2019-01-19 13:29:01 +0000

    Commit with the extra categories and pages added

--- populate_rango.py
+++ populate_rango.py
@@ -1,17 +1,13 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 
-					  'tango_with_django_project.settings')
-
+import os 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'tango_with_django_project.settings')
 
 import django
 django.setup()
-
-
-from rango.models import Category, Page
-
+from rango.models import Category
+from rango.models import Page
 
 def populate():
-
 	python_pages = [
 	{"title":"Official Python Tutorial",
 	"url":"http://docs.python.org/2/tutorial/"},
@@ -42,7 +38,7 @@ def populate():
 	 #Add extra categories or pages above this line if needed
 
 	for cat, cat_data in cats.items(): 
-		c= add_cat(cat)
+		c = add_cat(cat)
 		for p in cat_data["pages"]:
 			add_page(c, p["title"], p["url"])
 
@@ -51,21 +47,21 @@ def populate():
 		for p in Page.objects.filter(category=c):
 			print("- {0} - {1}".format(str(c), str(p)))
 
-	def add_page(cat, title, url, views=0):
-		p = Page.objects.get_or_create(category=cat, title=title)[0]
-		p.url=url
-		p.views=views
-		p.save()
-		return p
+def add_page(cat, title, url, views=0):
+	p = Page.objects.get_or_create(category=cat, title=title)[0]
+	p.url=url
+	p.views=views
+	p.save()
+	return p
 
-	def add_cat(name):
-		c = Category.objects.get_or_create(name=name)[0]
-		c.save()
-		return c
+def add_cat(name):
+	c = Category.objects.get_or_create(name=name)[0]
+	c.save()
+	return c
 
 	#Execution starts below
 
-	if __name__ == '__main__':
-		print("Starting Rango population script...") 
-		populate()
+if __name__ == '__main__':
+	print("Starting Rango population script...") 
+	populate()
 
